32de2c9847e1d9cae5cd437c572fa89d2509a466
ayetullah <ayetullahbm@gmail.com>
2020-10-21 00:01:01 +0300

    user ve part işlemleri düzenlendi.

---
+++ PaymentDetails/Migrations/20201019175416_AddPaymentDetail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaymentDetails.Migrations
+{
+    public partial class AddPaymentDetail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentDetails",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CardOwnerName = table.Column<string>(nullable: true),
+                    CardNumber = table.Column<string>(nullable: true),
+                    ExpirationDate = table.Column<string>(nullable: true),
+                    CVV = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentDetails", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentDetails");
+        }
+    }
+}
---
+++ PaymentDetails/PaymentDetails/Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using PaymentDetails.Models;
+
+namespace PaymentDetails.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly PdDbContex _context;
+
+        public AuthController(PdDbContex dbContex)
+        {
+            _context = dbContex;
+        }
+
+        [HttpPost, Route("login")]
+        public async Task<IActionResult> LoginAsync([FromBody] UserDto userDto)
+        {
+            if(userDto == null)
+            {
+                return BadRequest("Invalid client request");
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == userDto.UserName && x.Password == userDto.Password);
+            if(user != null)
+            {
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"));
+                var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(ClaimTypes.Role, "Administrator")
+                };
+                var tokenOptions = new JwtSecurityToken(
+                    issuer: "https://localhost:7500",
+                    audience: "https://localhost:7500",
+                    claims: claims,
+                    expires:DateTime.Now.AddMinutes(1),
+                    signingCredentials: signingCredentials);
+
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+                return Ok(new { Token = tokenString });
+            }
+
+            return Unauthorized();
+        }
+    }
+}
